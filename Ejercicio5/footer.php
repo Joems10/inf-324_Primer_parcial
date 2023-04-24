5057a34744e8cd1312db6d5322aa3f79d03357cf
Joems10 <130946115+Joems10@users.noreply.github.com>
2023-04-24 01:23:31 -0400

    Add files via upload

---
+++ Ejercicio5/footer.php
@@ -0,0 +1,11 @@
+
+      <!-- FOOTER -->
+      <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Joel Marcelo Soliz Caguaya 2023</p></div>
+        </footer>
+    </main>
+
+    <script src="<?php echo $nivel;?>js/bootstrap.bundle.min.js"></script>
+    
+  </body>
+</html>
